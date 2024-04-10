d7118438d07f4e137e5302aaa258fb889c108ad6
AntoineMbok <124931175+AntoineMbok@users.noreply.github.com>
2024-04-09 22:32:07 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -23,65 +23,54 @@
  * 
  */
 
+// scripts.js
 
-const FRESH_PRINCE_URL = "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
-const CURB_POSTER_URL = "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
-const EAST_LOS_HIGH_POSTER_URL = "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
-
-// This is an array of strings (TV show titles)
-let titles = [
-    "Fresh Prince of Bel Air",
-    "Curb Your Enthusiasm",
-    "East Los High"
+// Array of TV shows with title and poster URL
+let tvShows = [
+    {
+        title: "Fresh Prince of Bel Air",
+        posterURL: "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg",
+        bullets: ["Comedy", "Drama", "Sitcom"]
+    },
+    {
+        title: "Curb Your Enthusiasm",
+        posterURL: "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg",
+        bullets: ["Comedy", "Mockumentary", "Satire"]
+    },
+    {
+        title: "East Los High",
+        posterURL: "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg",
+        bullets: ["Drama", "Teen", "Romance"]
+    }
+    // Add more TV shows here
 ];
-// Your final submission should have much more data than this, and 
-// you should use more than just an array of strings to store it all.
 
-
-// This function adds cards the page to display the data in the array
 function showCards() {
     const cardContainer = document.getElementById("card-container");
+    const template = document.getElementById("card-template");
+
+    // Clear existing cards
     cardContainer.innerHTML = "";
-    const templateCard = document.querySelector(".card");
-    
-    for (let i = 0; i < titles.length; i++) {
-        let title = titles[i];
 
-        // This part of the code doesn't scale very well! After you add your
-        // own data, you'll need to do something totally different here.
-        let imageURL = "";
-        if (i == 0) {
-            imageURL = FRESH_PRINCE_URL;
-        } else if (i == 1) {
-            imageURL = CURB_POSTER_URL;
-        } else if (i == 2) {
-            imageURL = EAST_LOS_HIGH_POSTER_URL;
-        }
+    // Loop through TV shows and create a card for each
+    tvShows.forEach(show => {
+        const cardClone = template.content.cloneNode(true);
+        const card = cardClone.querySelector(".card");
 
-        const nextCard = templateCard.cloneNode(true); // Copy the template card
-        editCardContent(nextCard, title, imageURL); // Edit title and image
-        cardContainer.appendChild(nextCard); // Add new card to the container
-    }
+        card.querySelector("h2").textContent = show.title;
+        card.querySelector("img").src = show.posterURL;
+        card.querySelector("img").alt = show.title + " Poster";
+
+        const bulletsList = card.querySelector("ul");
+        show.bullets.forEach(bullet => {
+            const listItem = document.createElement("li");
+            listItem.textContent = bullet;
+            bulletsList.appendChild(listItem);
+        });
+
+        cardContainer.appendChild(card);
+    });
 }
-
-function editCardContent(card, newTitle, newImageURL) {
-    card.style.display = "block";
-
-    const cardHeader = card.querySelector("h2");
-    cardHeader.textContent = newTitle;
-
-    const cardImage = card.querySelector("img");
-    cardImage.src = newImageURL;
-    cardImage.alt = newTitle + " Poster";
-
-    // You can use console.log to help you debug!
-    // View the output by right clicking on your website,
-    // select "Inspect", then click on the "Console" tab
-    console.log("new card:", newTitle, "- html: ", card);
-}
-
-// This calls the addCards() function when the page is first loaded
-document.addEventListener("DOMContentLoaded", showCards);
 
 function quoteAlert() {
     console.log("Button Clicked!")
@@ -89,6 +78,10 @@ function quoteAlert() {
 }
 
 function removeLastCard() {
-    titles.pop(); // Remove last item in titles array
-    showCards(); // Call showCards again to refresh
+    tvShows.pop(); // Remove last TV show
+    showCards(); // Re-render cards
 }
+
+// Call showCards() when the page is loaded
+document.addEventListener("DOMContentLoaded", showCards);
+
